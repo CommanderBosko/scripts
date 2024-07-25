ea8ed24fa2fa5b27a50db7bb6d903e13bb5f75fe
CommanderBosko <kurthoernig@gmail.com>
2024-07-25 11:44:30 -0400

    Update after-install.sh

--- after-install.sh
+++ after-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the command to refresh package databases and install packages
-PACMAN_CMD="sudo pacman -Syy && sudo pacman -S --needed --noconfirm alacritty btop discord discover distrobox fastfetch firefox flatpak git gnome-boxes grub-customizer htop linux-lts linux-lts-headers linux-zen linux-zen-headers lutris nvidia-dkms nvidia-utils lib32-nvidia-utils podman qbittorrent starship steam-native-runtime thunderbird timeshift vlc wine wine-gecko wine-mono winetricks"
+PACMAN_CMD="sudo pacman -Syy && sudo pacman -S --needed --noconfirm alacritty btop discord discover distrobox fastfetch firefox flatpak git gnome-boxes grub-customizer htop linux-lts linux-lts-headers linux-zen linux-zen-headers lutris nvidia-dkms nvidia-utils lib32-nvidia-utils podman qbittorrent spotify-launcher starship steam-native-runtime thunderbird timeshift vlc wine wine-gecko wine-mono winetricks"
 
 # Create a temporary script to be executed in the terminal
 TEMP_SCRIPT=$(mktemp)
