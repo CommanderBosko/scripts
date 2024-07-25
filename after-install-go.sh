4448c1ea59fd9aa296f3f4b66f2dbb0c618dfeb5
CommanderBosko <kurthoernig@gmail.com>
2024-07-25 10:08:12 -0400

    Add files via upload

--- after-install-go.sh
+++ after-install-go.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # Define the command to install packages
-INSTALL_CMD="sudo pacman -S --needed --noconfirm linux-zen linux-zen-headers linux-lts linux-lts-headers nvidia-dkms nvidia-utils lib32-nvidia-utils grub-customizer alacritty firefox discord steam-native-runtime wine wine-gecko wine-mono winetricks lutris spotify-launcher discover flatpak fastfetch git vlc"
+pacman_list="sudo pacman -S --needed --noconfirm alacritty discord discover fastfetch firefox flatpak git grub-customizer linux-lts linux-lts-headers linux-zen linux-zen-headers lutris nvidia-dkms nvidia-utils lib32-nvidia-utils steam-native-runtime wine wine-gecko wine-mono winetricks"
 
 # Check for the default terminal emulator
 if command -v xdg-terminal &> /dev/null; then
-    xdg-terminal -e "$INSTALL_CMD"
+    xdg-terminal -e "$pacman_list"
 elif command -v gnome-terminal &> /dev/null; then
-    gnome-terminal -- bash -c "$INSTALL_CMD; exec bash"
+    gnome-terminal -- bash -c "$pacman_list; exec bash"
 elif command -v konsole &> /dev/null; then
-    konsole -e bash -c "$INSTALL_CMD; exec bash"
+    konsole -e bash -c "$pacman_list; exec bash"
 elif command -v xfce4-terminal &> /dev/null; then
-    xfce4-terminal -e "$INSTALL_CMD"
+    xfce4-terminal -e "$pacman_list"
 elif command -v xterm &> /dev/null; then
-    xterm -e "$INSTALL_CMD"
+    xterm -e "$pacman_list"
 elif command -v lxterminal &> /dev/null; then
-    lxterminal -e "$INSTALL_CMD"
+    lxterminal -e "$pacman_list"
 else
     echo "No compatible terminal emulator found!"
     exit 1
